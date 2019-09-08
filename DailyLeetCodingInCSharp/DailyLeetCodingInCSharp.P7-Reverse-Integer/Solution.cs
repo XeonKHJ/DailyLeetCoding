71d9e0ed32cce76f4b401f5567b7f0d4f67a18a8
康宏嘉 <redalertkhj@live.cn>
2019-09-08 20:34:41 +0800

    完成第七题。

---
+++ DailyLeetCodingInCSharp/DailyLeetCodingInCSharp.P7-Reverse-Integer/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DailyLeetCodingInCSharp.P7_Reverse_Integer
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            int maxValueWithoutFirstDigit = int.MaxValue / 10;
+            int maxFirstBit = int.MaxValue % 10;
+            int minValueWithoutFirstDigit = int.MinValue / 10;
+            int minFirstBit = int.MinValue % 10;
+            int result = 0;
+            while (x != 0)
+            {
+                int remain = x % 10;
+
+                if (x > 0 &&
+                    ((result > maxValueWithoutFirstDigit) ||
+                     (result == maxValueWithoutFirstDigit && remain > maxFirstBit)))
+                {
+                    return 0;
+                }
+                else if (x < 0 &&
+                       (result < minValueWithoutFirstDigit ||
+                       (result == minValueWithoutFirstDigit && remain < minFirstBit)))
+                {
+                    return 0;
+                }
+
+                result = result * 10 + remain;
+                x /= 10;
+            }
+
+
+
+            return result;
+        }
+    }
+}
---
+++ DailyLeetCodingInCSharp/DailyLeetCodingInCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DailyLeetCodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            P7_Reverse_Integer.Solution solution = new P7_Reverse_Integer.Solution();
+            Console.WriteLine(solution.Reverse(23445));
+        }
+    }
+}
